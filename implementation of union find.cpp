96aec3e2e7639932071a26c283adf5b7a96cdf66
krishna-pandey-beep <72220911+krishna-pandey-beep@users.noreply.github.com>
2020-10-01 23:11:30 +0530

    Create implementation of union find.cpp

---
+++ implementation of union find.cpp
@@ -0,0 +1,28 @@
+//Implementation of union find;
+
+
+//arr pp[] is to manage parent of elements;
+//arr size[] is to store size of each disjoint set---initially 1 is stored;
+
+
+//find function;
+int find(int a)
+{
+    if(pp[a]==a)
+    return a;
+    return pp[a]=find(pp[a]);  //parent changes to the root parent while function returns;
+}
+
+//union function;
+void union(int a,int b)
+{
+    a=find(a);
+    b=find(b);
+    if(a!=b)
+    {
+       if(size[a]<size[b])
+       swap(a,b);
+       pp[b]=a;            //we always attach set having smaller size with the set having larger size;
+       size[a]+=size[b];
+    }
+}
